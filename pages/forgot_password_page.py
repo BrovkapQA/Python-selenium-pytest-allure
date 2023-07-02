3d507ca0208bf3be3205636e0d6ab3be22365c1f
https://github.com/BrovkapQA <berezko.qa@yandex.ru>
2023-07-03 01:36:23 +0400

    added allure steps

--- pages/forgot_password_page.py
+++ pages/forgot_password_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 
 from pages.base_page import BasePage
@@ -16,15 +17,19 @@ class ForgotPasswordPage(BasePage):
     INVALID_EMAIL_MESSAGE = (By.XPATH, '//span[@class="flush-form"]')
     NOT_REGISTERED_USER_MESSAGE = (By.XPATH, '//div[@class="alert alert-primary"]')
 
+    @allure.step("Sending a link to restore the password")
     def send_password_reset_link(self, email):
         self.send_keys(self.FILD_EMAIL, email)
         self.click(self.CONFIRM_BUTTON)
 
+    @allure.step("Message about successful password reset")
     def get_success_message(self):
         return self.get_text(self.SUCCESS_MESSAGE)
 
+    @allure.step("Message about a non-correct email")
     def get_invalid_email_message(self):
         return self.get_text(self.INVALID_EMAIL_MESSAGE)
 
+    @allure.step("Message about an unregistered email")
     def get_not_registered_user_message(self):
         return self.get_text(self.NOT_REGISTERED_USER_MESSAGE)
--- tests/test_forgot_password.py
+++ tests/test_forgot_password.py
@@ -3,22 +3,28 @@ import allure
 from tests.test_base import BaseTest
 
 
-@allure.severity("Blocker")
+@allure.severity("Critical")
 @allure.feature("Forgot password")
 class TestForgotPassword(BaseTest):
 
+    @allure.description("Forgot password valid email")
+    @allure.title("Forgot Password with valid email test")
     def test_valid_email(self, login_page, forgot_password_page):
         login_page.click_forgot_password()
         forgot_password_page.send_password_reset_link("testuser@gmail.com")
         assert "Проверьте вашу электронную почту для дальнейших инструкций по сбросу пароля" == \
                forgot_password_page.get_success_message()
 
+    @allure.description("Forgot password with not exist email")
+    @allure.title("Forgot Password with an email that does not exist in the system")
     def test_not_a_registered_user(self, login_page, forgot_password_page):
         login_page.click_forgot_password()
         forgot_password_page.send_password_reset_link("000@gmail.com")
         assert "Пользователь с таким email не зарегистрирован" == \
                forgot_password_page.get_not_registered_user_message()
 
+    @allure.description("Forgot password with invalid email")
+    @allure.title("Forgot Password with an invalid email address")
     def test_invalid_email(self, login_page, forgot_password_page):
         login_page.click_forgot_password()
         forgot_password_page.send_password_reset_link("123e")
