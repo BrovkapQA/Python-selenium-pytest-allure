ce8cd59182fab6fbd24c300a3cac2ce424a817c2
https://github.com/BrovkapQA <berezko.qa@yandex.ru>
2023-07-08 19:10:30 +0400

    added a utility for reading test data from excel

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,19 +4,19 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
 
-    def __init__(self, driver, wait: WebDriverWait):
+    def __init__(self, driver):
         self.driver = driver
-        self.wait = wait
+
 
 
     def click(self, locator):
-        return self.wait.until(EC.element_to_be_clickable(locator)).click()
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator)).click()
 
     def send_keys(self, locator, txt):
-        return self.wait.until(EC.element_to_be_clickable(locator)).send_keys(txt)
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator)).send_keys(txt)
 
     def clear_text(self, locator):
-        return self.wait.until(EC.visibility_of_element_located(locator))
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
 
     def get_text(self, locator):
-        return self.wait.until(EC.visibility_of_element_located(locator)).text
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator)).text
