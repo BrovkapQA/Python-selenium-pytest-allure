4026037b05ab683d15fd3cf2bfb15cfe8ac1e0f6
https://github.com/BrovkapQA <berezko.qa@yandex.ru>
2023-07-14 00:12:10 +0400

    added and check the action for nav_menu_page

--- tests/test_check.py
+++ tests/test_check.py
@@ -14,9 +14,26 @@ class TestCheck:
         login_page = LoginPage(self.driver)
         nav_menu_page = NavMenuPage(self.driver)
 
-        login_page.login("Visoveny@gmail.com", "Asd123df")
+        login_page.login("", "")
         nav_menu_page.get_messanger()
+        nav_menu_page.get_notification()
         nav_menu_page.get_calendar()
+        nav_menu_page.get_seans_suggestions()
+        nav_menu_page.get_techniques()
+        nav_menu_page.get_notice()
+        nav_menu_page.get_akpp()
+        nav_menu_page.get_events()
+        nav_menu_page.get_certificates()
+        nav_menu_page.get_courses()
+        nav_menu_page.get_supervision_without_courses()
+        nav_menu_page.get_personal_account_individual_therapies()
+        nav_menu_page.get_individual_supervision()
+        nav_menu_page.get_techniques_and_skills()
+        nav_menu_page.get_testing()
+        nav_menu_page.get_intervision()
+        nav_menu_page.get_finance()
+        time.sleep(5)
+
 
 
 
